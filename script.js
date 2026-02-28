f397f36abffa1bd45f602cfed35bb3a538ce4547
Soujanya A R <soujanyapoojary80@gmail.com>
2026-02-28 21:02:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+//This section selects all required HTML elements for the music player
+const audioPlayer = document.getElementById('audioPlayer');
+const playPauseButton = document.getElementById('playPauseButton');
+const nextButton = document.getElementById('nextButton');
+const prevButton = document.getElementById('prevButton');
+const trackNameElement = document.getElementById('trackName');
+const imageElement = document.getElementById('image');
+const volumeControl = document.getElementById('volumeControl');
+
 
