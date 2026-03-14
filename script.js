4e77f7f03d33e3929ae5523ccd9e6ca6b6aaaff8
Soujanya A R <soujanyapoojary80@gmail.com>
2026-03-14 20:12:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,38 +30,6 @@ const audioFiles = [
 let currentTrack = 0;
 let isPlaying = false;
 
-//This section selects all required HTML elements for the music player
-const audioPlayer = document.getElementById('audioPlayer');
-const playPauseButton = document.getElementById('playPauseButton');
-const nextButton = document.getElementById('nextButton');
-const prevButton = document.getElementById('prevButton');
-const trackNameElement = document.getElementById('trackName');
-const imageElement = document.getElementById('image');
-const volumeControl = document.getElementById('volumeControl');
-
-// Music playlist (song name, audio file, image)
-const audioFiles = [
-    {name:'EEGA Nene Ne audio Song',
-     src:'music/EEGA  Nene Nani Ne Video Song.mp3', 
-     img:'image/eega.jpg'
-    },
-     {name:'Love Mocktail - Neene Yendigu',
-     src:'music/Love Mocktail - Neene Yendigu (Video Song)  Krishna, Milana  Raghu Dixit  Raghavendra Kamath.mp3', 
-     img:'image/love_Mocktail.jpg'
-    },
-    {name:'Olave Olave - Lyrical  SSE (Side B)g',
-     src:'music/Olave Olave - Lyrical  SSE (Side B)  Rakshit Shetty, Rukmini  Charan RajHemanth MRao.mp3',
-     img:'image/saptha.jpg'
-    },
-    {name:'Ranjha  Official Video  Shershaah g',
-     src:'music/Ranjha  Official Video  Shershaah  SidharthKiara  B Praak  Jasleen Royal  Romy  Anvita Dutt.mp3',
-     img:'image/shershaah.jpg'
-    },
-];
-
-let currentTrack = 0;
-let isPlaying = false;
-
 // Function to load selected track
 function loadTrack(index){
     audioPlayer.src = audioFiles[index].src;
@@ -113,61 +81,6 @@ loadTrack(currentTrack);
 
 audioPlayer.volume = volumeControl.value;
 
-volumeControl.addEventListener("input",() => {
-    audioPlayer.volume = volumeControl.value;
-    volumeControl.textContent = ` volume:${Math.round(volumeControl.value*100)}%`
-});
-function loadTrack(index){
-    audioPlayer.src = audioFiles[index].src;
-    trackNameElement.textContent = audioFiles[index].name;
-    imageElement.src = audioFiles[index].img;
-
-    if (isPlaying) {
-        audioPlayer.play();
-    }
-}
-
-//function to toggle play\pause
-function togglePlayPause() {
-if (isPlaying) {
-     audioPlayer.pause();
-     isPlaying = false;
-     playPauseButton.textContent = '\u25B6';//playicon
-
-    }
-
-else{
-    audioPlayer.play();
-     isPlaying=true;
-     playPauseButton.textContent = '\u275A\u275A';
-
-     }
-}
-
-playPauseButton.addEventListener('click', () => { 
-    togglePlayPause();
-});
-
-nextButton.addEventListener('click', () => {
-        currentTrack= (currentTrack + 1)%audioFiles.length;
-        loadTrack(currentTrack);
-});
-
-prevButton.addEventListener('click', () => {
-    currentTrack = (currentTrack- 1 + audioFiles.length) % audioFiles.length;
-    loadTrack(currentTrack);
-});
-
-audioPlayer.addEventListener('ended',() => {
-    currentTrack = (currentTrack + 1)% audioFiles.length;
-    loadTrack(currentTrack);
-});
-
-loadTrack(currentTrack);
-
-audioPlayer.volume = volumeControl.value;
-
-//volume increase or decrease
 volumeControl.addEventListener("input",() => {
     audioPlayer.volume = volumeControl.value;
     volumeControl.textContent = ` volume:${Math.round(volumeControl.value*100)}%`
